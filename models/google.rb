3edc7b1029b3412e70fd684c0ebea535756d0fca
RishiGit <rishikrishnan@hotmail.com>
2015-01-24 17:12:39 -0500

    Can compare search results.

---
+++ models/google.rb
@@ -0,0 +1,39 @@
+require 'mechanize'
+
+class Google
+
+	def initialize(first_word, second_word)
+		mechanize = Mechanize.new
+
+		@first_page = mechanize.get("https://www.google.com/search?q=#{first_word}")
+		@second_page = mechanize.get("https://www.google.com/search?q=#{second_word}")
+	end
+
+	def get_popularity_1
+		@page1_stat = @first_page.at('#resultStats').text.strip
+		@word1_popularity = @page1_stat.scan(/\d/).join('').to_i
+		puts @word1_popularity
+	end
+
+	def get_popularity_2
+		@page2_stat = @second_page.at('#resultStats').text.strip
+		@word2_popularity = @page2_stat.scan(/\d/).join('').to_i
+		puts @word2_popularity
+	end
+
+	def compare
+		if @word1_popularity >= @word2_popularity
+			puts "Word 1 is more popular!!!"
+		elsif @word1_popularity <= @word2_popularity
+			puts "Word 2 is more popular!!!"
+		else @word1_popularity == @word2_popularity
+			puts "Both Words are equally popular"
+		end
+	end
+
+end
+
+test = Google.new("right twix","left twix")
+test.get_popularity_1
+test.get_popularity_2
+test.compare
--- models/twitterapi.rb
+++ models/twitterapi.rb
@@ -1,22 +1,46 @@
 require 'twitter'
+require 'pry'
 
 class TweetInfo
-	def initialize
-		client = Twitter::REST::Client.new do |config|
+	attr_accessor :client 
+	def initialize(word1, word2)
+		@client = Twitter::REST::Client.new do |config|
 		  config.consumer_key        = "l1NsZBe85y8vFJoqCBOkwTRgo"
 		  config.consumer_secret     = "3ifDWX78CP8OrXVVMnSfcnwhcNXEgKfVwWHAwGMuzflm6b4zA6"
 		  config.access_token        = "377725157-FVfjlycF7EKlxocRLv4dS8RauIjdqBS44fGXhNR0"
 		  config.access_token_secret = "gcFcL5T6Nnw5sfloQrKWYrzJUChur5JYenzIYgnHWmvLq"
 		end
+
+		@word1 = word1
+		@word2 = word2
 	end
 
-	client.search("to:justinbieber marry me", result_type: "recent").take(3).collect do |tweet|
-		puts "#{tweet.user.screen_name}: #{tweet.text}"
+	def word1
+		@word1pop =  @client.search(@word1, result_type: "mixed").count
+	end
+	def word2
+		@word2pop =  @client.search(@word2, result_type: "mixed").count
+	end
+
+	def compare
+		if @word1pop >= @word2pop
+			puts "Word 1 is more popular!!!"
+		elsif @word1pop <= @word2pop
+			puts "Word 2 is more popular!!!"
+		else @word1pop == @word2pop
+			puts "Both Words are equally popular"
+		end
 	end
 end
 
-word1 = TweetInfo.new
-word2 = TweetInfo.ew
+	test = TweetInfo.new("flatiron","pizza")
+	test.word1
+	test.word2
+	binding.pry
+
+	
+
+
 
 
 
